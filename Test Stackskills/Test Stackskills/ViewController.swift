1908ff851573095d00b5cae6fac1f3661ac4f872
Michael Lee <shmee1717@hotmail.com>
2017-02-27 15:56:07 -1000

    Adding text1 and text2

--- Test Stackskills/Test Stackskills/ViewController.swift
+++ Test Stackskills/Test Stackskills/ViewController.swift
@@ -12,22 +12,21 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var coolLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
+    
+    
+    
     
     @IBAction func buttonTapped(_ sender: Any) {
-        tapCount = tapCount + 1
-        print(tapCount)
-        if tapCount >= 20 {
-            coolLabel.text = "You tapped the button ten times!"
-        }
-    
-    }
-   
-    
-    @IBAction func buttonTappedAgain(_ sender: Any) {
-        coolLabel.text = "Hi Leah Again!"
+        
+        print(text1.text!)
+        print(text2.text!)
         
     }
+   
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
